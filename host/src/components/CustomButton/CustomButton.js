d87499b34bbc22319152dd36e22887aab0763742
marchuk <marchuk@ddplanet.ru>
2025-07-24 16:15:24 +0300

    Правки по tsconfig добавил "outDir": "./dist", удалил вручную скомпилированные js. Так же добавил файлы и папки которые не нужно включать в проверку TS - "exclude": ["*.js", "node_modules", "dist"]

--- host/src/components/CustomButton/CustomButton.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import "./style.scss";
-export const CustomButton = (props) => {
-    return (_jsx("button", { onClick: props.onClick, children: "\u041A\u043D\u043E\u043F\u043E\u0447\u043A\u0430" }));
-};
